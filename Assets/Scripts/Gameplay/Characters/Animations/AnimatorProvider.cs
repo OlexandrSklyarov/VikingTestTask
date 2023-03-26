482e102794ece7bc4715892ebebb9d062a7d0199
Sklyarov Olexandr <alexskrat@gmail.com>
2023-03-26 16:20:43 +0300

    [Add] Added animator provider.

---
+++ Assets/Scripts/Gameplay/Characters/Animations/AnimatorProvider.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+namespace Gameplay.Characters.Animations
+{
+    [RequireComponent(typeof(Animator))]
+    public class AnimatorProvider : MonoBehaviour
+    {
+        public Animator Animator => _animator;
+        
+        private Animator _animator;
+        
+        public event Action AttackEvent;
+
+        public void Init() => _animator = GetComponent<Animator>();
+
+        
+        private void OnAttack() => AttackEvent?.Invoke();
+        
+        
+        public void SetSpeed(float speed) => 
+                _animator.SetFloat(ConstPrm.Animation.MOVE_SPEED_PRM, speed, 0.1f, Time.deltaTime);
+
+        
+        public void PlayAttack() => SetTrigger(ConstPrm.Animation.ATTACK);
+        
+        
+        public void PlayDamage() => SetTrigger(ConstPrm.Animation.DAMAGE);
+        
+        
+        public void PlayDie() => SetTrigger(ConstPrm.Animation.DIE);
+        
+
+        private void SetTrigger(string triggerName) => _animator.SetTrigger(triggerName);
+    }
+}
